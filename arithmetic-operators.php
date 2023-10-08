19aa9b06347b4900fc34bb158af08273a7cd164e
Leonardo Henrique Manzini <leonardomanzini@castgrouppartner.com.br>
2023-10-08 18:01:35 -0300

    lesson 5: studing some PHP arithmetic operations

---
+++ arithmetic-operators.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP arithmetic operators</title>
+</head>
+<body>
+	<?php 
+		echo "Addition: ". 5 + 5;
+		echo "<br>";
+
+		echo "Subtraction: ". 5 - 5;
+		echo "<br>";
+
+		echo "Multiplication: ". 5 * 5;
+		echo "<br>";
+
+		echo "Division: ". 5 / 5;
+		echo "<br>";
+
+		echo "Modulus: ". 8 % 5;
+		echo "<br>";
+
+		echo "Exponentiation: ". 5 ** 5;
+		echo "<br>";
+	?>
+</body>
+</html>
