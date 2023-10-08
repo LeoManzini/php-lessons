3ea8d97baf424ee93a5460b741fabdd7a8b6a98a
Leonardo Henrique Manzini <leonardomanzini@castgrouppartner.com.br>
2023-10-08 18:44:21 -0300

    lesson 10: PHP elseif statement

---
+++ conditional-statements.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP conditional statements</title>
+</head>
+<body>
+	<?php 
+		$x = 1;
+
+		if ($x == 1) {
+			echo "Leonardo is very handsome!";
+		} elseif ($x == 2) {
+			echo "Leonardo is kinda handsome!";
+		} else {
+			echo "Leonardo, is very ugly!";
+		}
+	?>
+</body>
+</html>
