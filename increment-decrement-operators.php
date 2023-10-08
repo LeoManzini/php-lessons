382e1cbbd059f86077d33d5cc682d57bfdfdfa81
Leonardo Henrique Manzini <leonardomanzini@castgrouppartner.com.br>
2023-10-08 18:27:22 -0300

    lesson 8: increment and decrement with PHP

---
+++ increment-decrement-operators.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP increase decrease operators</title>
+</head>
+<body>
+	<?php 
+		$x = 10;
+		echo $x;
+		echo "<br>";
+
+		// Post incrementing
+		$x++;
+		echo $x;
+		echo "<br>";
+
+		// Pre incrementing
+		++$x;
+		echo $x;
+		echo "<br>";
+
+		// Post decrement
+		$x--;
+		echo $x;
+		echo "<br>";
+
+		// Pre decrement
+		--$x;
+		echo $x;
+		echo "<br>";
+	?>
+</body>
+</html>
