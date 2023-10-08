a0c34616e3b710738309ffe0ca52a6f967fa8176
Leonardo Henrique Manzini <leonardomanzini@castgrouppartner.com.br>
2023-10-08 17:15:35 -0300

    First lesson

---
+++ print.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Printing lesson</title>
+</head>
+<body>
+	<?php
+		# PHP tem ponto e vígula no final
+		echo "Hello World!";
+		// Print também funciona como imprimindo na tela
+		print("<br>Hello World, again!");
+	?>
+</body>
+</html>
