38896f41b6c12a8c0f87ba0608b333be9797b594
Leonardo Henrique Manzini <leonardomanzini@castgrouppartner.com.br>
2023-10-08 18:07:15 -0300

    lesson 6: PHP assignment operations

---
+++ assignment-operators.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP assignment operators</title>
+</head>
+<body>
+	<?php 
+		$x = 100;
+
+		// Addition
+		$x += 1;
+
+		// Subtraction
+		$x -= 1;
+
+		// Multiplication
+		$x *= 5;
+
+		// Division
+		$x /= 5;
+
+		// Modulus
+		$x %= 5;
+
+		// Exponentiation
+		$x **= 5;
+
+		echo $x;
+	?>
+</body>
+</html>
