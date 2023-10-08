a94ff2c10a264729622276ccf999e06aba282b1e
Leonardo Henrique Manzini <leonardomanzini@castgrouppartner.com.br>
2023-10-08 18:49:02 -0300

    lesson 11: PHP switch statement

---
+++ switch-statements.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP switch statements</title>
+</head>
+<body>
+	<?php 
+		$x = 1;
+
+		switch ($x) {
+			case 1:
+				echo "Uow, you are handsome!";
+				break;
+			case 2:
+				echo "Yeah, you are acceptable!";
+				break;
+			default:
+				echo "Uow, you are an ugly motherfuck**!";
+				break;
+		}
+	?>
+</body>
+</html>
